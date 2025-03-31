e0c9b2a236f0258a329dee5faadbecd7aedf74eb
egrozell <ethan.grozell@ucalgary.ca>
2025-03-31 00:46:18 -0600

    minor formating

--- performance_test.py
+++ performance_test.py
@@ -8,7 +8,7 @@ u = TimeFunction(name='u', grid=grid, space_order=2, save=200)
 c = Constant(name='c')
 eqn = Eq(u.dt, c * u.laplace)
 step = Eq(u.forward, solve(eqn, u.forward))
-op = Operator([step])
+op = Operator([step], platform='nvidiaX', opt=('advanced', {'gpu-fit': u}))
 xx, yy = np.meshgrid(np.linspace(0., 1., nx, dtype=np.float32),
                      np.linspace(0., 1., ny, dtype=np.float32))
 r = (xx - .5)**2. + (yy - .5)**2.
@@ -18,5 +18,6 @@ stats = op.apply(dt=5e-05, c=0.5)
 plt.rcParams['figure.figsize'] = (20, 20)
 for i in range(1, 6):
     plt.subplot(1, 6, i)
-    plt.imshow(u.data[(i-1)*40])
+    plt.imshow(u.data[(i - 1) * 40])
 plt.savefig("figures/figure.svg")
+print(op)
