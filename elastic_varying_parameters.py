f3b7e60e8b6644d593952afcbe7878cdec36689a
egrozell <ethan.grozell@ucalgary.ca>
2025-04-07 01:40:22 -0600

    started working on an elastic implementation based on tutorial 6

---
+++ elastic_varying_parameters.py
@@ -0,0 +1,50 @@
+import devito
+from examples.seismic.source import RickerSource, Receiver, TimeAxis
+from src.final_project.model import demo_model
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from sympy import init_printing, latex
+init_printing(use_latex='mathjax')
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('xtick', labelsize=20)
+plt.rc('ytick', labelsize=20)
+
+nlayers = 5
+so = 8
+model = demo_model(vp_top=1.0, vp_bottom=3.5, nlayers=nlayers,
+                   shape=(301, 301), spacing=(10., 10.), space_order=so)
+aspect_ratio = model.shape[0] / model.shape[1]
+plt_config = {'cmap': 'jet', 'extent': [model.origin[0], model.origin[0] + model.domain_size[0],
+                                        model.origin[1] + model.domain_size[1], model.origin[1]]}
+
+fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
+slices = [slice(model.nbl, -model.nbl), slice(model.nbl, -model.nbl)]
+
+img1 = ax[0].imshow(np.transpose(model.lam.data[slices]), vmin=1.5**2, vmax=4.0**2, **plt_config)
+fig.colorbar(img1, ax=ax[0])
+ax[0].set_title(r"First Lam\'e parameter $\lambda$", fontsize=20)
+ax[0].set_xlabel('X (m)', fontsize=20)
+ax[0].set_ylabel('Depth (m)', fontsize=20)
+ax[0].set_aspect('auto')
+
+
+img2 = ax[1].imshow(np.transpose(model.mu.data[slices]), vmin=0, vmax=15, **plt_config)
+fig.colorbar(img2, ax=ax[1])
+ax[1].set_title(r"Shear modulus $\mu$", fontsize=20)
+ax[1].set_xlabel('X (m)', fontsize=20)
+ax[1].set_ylabel('Depth (m)', fontsize=20)
+ax[1].set_aspect('auto')
+
+
+img3 = ax[2].imshow(1 / np.transpose(model.b.data[slices]), vmin=1.0, vmax=3.0, **plt_config)
+fig.colorbar(img3, ax=ax[2])
+ax[2].set_title(r"Density $\rho$", fontsize=20)
+ax[2].set_xlabel('X (m)', fontsize=20)
+ax[2].set_ylabel('Depth (m)', fontsize=20)
+ax[2].set_aspect('auto')
+
+plt.savefig("./figures/elastic_model_physical_properties.jpeg")
+plt.tight_layout()
---
+++ src/final_project/model.py
@@ -0,0 +1,45 @@
+from examples.seismic.model import SeismicModel
+import numpy as np
+
+
+def Gardners(vp, normalize=True):
+    """
+    Gardner's relation for vp in km/s
+    """
+    b = 1 / (0.31 * (1e3 * vp)**0.25)
+    if normalize:
+        b[vp < 1.51] = 1.0
+    return b
+
+
+def demo_model(vp_top=1.5, vp_bottom=3.5, **kwargs):
+    space_order = kwargs.pop('space_order', 2)
+    shape = kwargs.pop('shape', (101, 101))
+    spacing = kwargs.pop('spacing', tuple([10. for _ in shape]))
+    origin = kwargs.pop('origin', tuple([0. for _ in shape]))
+    nbl = kwargs.pop('nbl', 10)
+    dtype = kwargs.pop('dtype', np.float32)
+    nlayers = kwargs.pop('nlayers', 3)
+
+    # A n-layers model in a 2D or 3D domain with two different
+    # velocities split across the height dimension:
+    # By default, the top part of the domain has 1.5 km/s,
+    # and the bottom part of the domain has 2.5 km/s.
+    vp_top = kwargs.pop('vp_top', vp_top)
+    vp_bottom = kwargs.pop('vp_bottom', vp_bottom)
+    # Define a velocity profile in km/s
+    v = np.empty(shape, dtype=dtype)
+    v[:] = vp_top  # Top velocity (background)
+    vp_i = np.linspace(vp_top, vp_bottom, nlayers)
+    for i in range(1, nlayers):
+        v[..., i * int(shape[-1] / nlayers):] = vp_i[i]  # Bottom velocity
+
+        vs = 0.5 * v[:]
+        b = Gardners(v)
+        vs[v < 1.51] = 0.0
+
+    return SeismicModel(space_order=space_order, vp=v, vs=vs, b=b,
+                        origin=origin, shape=shape,
+                        dtype=dtype, spacing=spacing, nbl=nbl, **kwargs)
+
+
